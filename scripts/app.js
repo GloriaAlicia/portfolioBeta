4d74444bfb524da4aa2e32dcb2c76cbb4e128766
Gloria <ga365743@gmail.com>
2022-06-22 17:06:09 -0500

    send email with API

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,48 @@
-let openMobil = document.getElementById("openMobil");
-let contentMenu = document.getElementById("content");
+const openMobil = document.getElementById("openMobil");
+const contentMenu = document.getElementById("content");
+const $form = document.getElementById("form");
+const $sendButton = document.getElementById("sendButton");
 
 
 openMobil.addEventListener("click", ()=>{
-    console.log("abrir");
-
     contentMenu.classList.toggle("hidden");
 })
+/***************validar */
+function noEmptyData(inputs){
+    if (inputs.length != 0){
+        return  true;
+     } else {
+        return false
+     }
+}
+
+$sendButton.addEventListener("click",()=>{
+    if(noEmptyData($form.name.value) && noEmptyData($form.email.value) && noEmptyData($form.message.value)){
+        alert("bien")
+        // $sendButton.disabled = false;
+    
+    } else {
+        alert("no estan llenos todos los campos")
+        // $sendButton.disabled = true;
+    }
+})
+
+$form.addEventListener("submit",(e)=>{
+    sendMenssage(e);
+});
+
+async function sendMenssage(e){
+    e.preventDefault()
+    const info = new FormData(e.target)
+    const response = await fetch(e.target.action,{
+        method: $form.method,
+        body: info,
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+    if(response.ok){
+        alert("Gracias por contactar :)")
+        $form.reset()
+    }
+}
